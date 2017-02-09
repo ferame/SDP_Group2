be01de7e68abc7709a23204e2e89dcdb5927ced8
Miroslav Trifonov <s1443569@buttermere.inf.ed.ac.uk>
2017-02-09 12:13:33 +0000

    Rotation now works

--- Fred_Stefan_modified/src/strategy/controllers/essentials/MotionController.java
+++ Fred_Stefan_modified/src/strategy/controllers/essentials/MotionController.java
@@ -31,7 +31,6 @@ public class MotionController extends ControllerBase {
 
     private int haveBall = 1;
 
-
     private int tolerance;
 
     private LinkedList<Obstacle> obstacles = new LinkedList<Obstacle>();
@@ -40,68 +39,52 @@ public class MotionController extends ControllerBase {
         super(robot);
     }
 
-    public enum MotionMode {
+    public enum MotionMode{
         ON, OFF
     }
 
-    public void setMode(MotionMode mode) {
+    public void setMode(MotionMode mode){
         this.mode = mode;
     }
 
-    public void setTolerance(int tolerance) {
+    public void setTolerance(int tolerance){
         this.tolerance = tolerance;
     }
 
-    public int getTolerance() {
+    public int getTolerance(){
         return this.tolerance;
     }
 
-    public void setDestination(DynamicPoint destination) {
+    public void setDestination(DynamicPoint destination){
         this.destination = destination;
     }
 
-    public void setHeading(DynamicPoint dir) {
+    public void setHeading(DynamicPoint dir){
         this.heading = dir;
     }
 
-    public void addObstacle(Obstacle obstacle) {
+    public void addObstacle(Obstacle obstacle){
         this.obstacles.add(obstacle);
     }
 
-    public void clearObstacles() {
+    public void clearObstacles(){
         this.obstacles.clear();
     }
 
-    public void perform() {
-        if (this.mode == MotionMode.OFF) return;
+    public void perform(){
+        if(this.mode == MotionMode.OFF) return;
 
         Robot us = Strategy.world.getRobot(RobotType.FRIEND_2);
-        if (us == null) return;
+        if(us == null) return;
 
-        //TODO haveball variable need to be checked and changed, using sonar
-        if (haveBall == 0) {
-            move(us);
-        } else if (haveBall == 1){
-            rotate(us);
-        }
-        else  if (haveBall == 2) {
-            kick(us);
-        }
-        else {
-            System.out.println("Error in haveball var = " + haveBall);
-        }
-
-
-    }
-
-    private void move(Robot us) {
         NavigationInterface navigation;
 
         VectorGeometry heading = null;
         VectorGeometry destination = null;
 
 
-        if (this.destination != null) {
+
+        if(this.destination != null){
             this.destination.recalculate();
 
             destination = new VectorGeometry(this.destination.getX(), this.destination.getY());
@@ -109,60 +92,63 @@ public class MotionController extends ControllerBase {
             boolean intersects = false;
 
 
-            for (Obstacle o : this.obstacles) {
+            for(Obstacle o : this.obstacles){
                 intersects = intersects || o.intersects(us.location, destination);
                 //System.out.println("Check Obstacle");
             }
 
-            for (Robot r : Strategy.world.getRobots()) {
-                if (r != null && r.type != RobotType.FRIEND_2) {
+            for(Robot r : Strategy.world.getRobots()){
+                if(r != null && r.type != RobotType.FRIEND_2){
                     intersects = intersects || VectorGeometry.vectorToClosestPointOnFiniteLine(us.location, destination, r.location).minus(r.location).length() < 30;
                     //System.out.println("Found potential obstacle");
                 }
                 //System.out.println("Check potential obstacle");
 
             }
-            // Robot is moving towards the ball
-            if ( //intersects ||
-                    (us.location.distance(destination) > 40)) {
+            // Robot is moving towards the ball. Why is "intersects" commented and what does it do ?
+            if( /*intersects ||  us.location.distance(destination) > 50*/ false){
+                haveBall = 0;
                 navigation = new AStarNavigation();
                 navigation.setHeading(destination);
                 GUI.gui.searchType.setText("A*");
-                System.out.println("A* Prop down");
+                //System.out.println("A* Prop down");
 
-                ((Fred) this.robot).PROPELLER_CONTROLLER.setActive(false);
+                ((Fred)this.robot).PROPELLER_CONTROLLER.setActive(false);
                 ((FredRobotPort) this.robot.port).propeller(0);
                 ((FredRobotPort) this.robot.port).propeller(0);
                 ((FredRobotPort) this.robot.port).propeller(0);
 
-            }
+            } else
             // Robot is getting close to the ball
-            else if (us.location.distance(destination) > 21 && us.location.distance(destination) < 40) {
-                navigation = new AStarNavigation();
-                navigation.setHeading(destination);
-                GUI.gui.searchType.setText("A*");
-                System.out.print("A* Prop up ");
-                System.out.println(us.location.distance(destination));
-                ((Fred) this.robot).PROPELLER_CONTROLLER.setActive(true);
-                ((FredRobotPort) this.robot.port).propeller(100);
-                ((FredRobotPort) this.robot.port).propeller(100);
-                ((FredRobotPort) this.robot.port).propeller(100);
+            {
+//                if( us.location.distance(destination) > 22 && us.location.distance(destination) < 50){
+                if (false) {
+                    haveBall = 0;
+                    navigation = new AStarNavigation();
+                    navigation.setHeading(destination);
+                    GUI.gui.searchType.setText("A*");
+                    System.out.print("A* Prop up ");
+                    System.out.println( us.location.distance(destination));
+                    ((Fred)this.robot).PROPELLER_CONTROLLER.setActive(true);
+                    ((FredRobotPort) this.robot.port).propeller(100);
+                    ((FredRobotPort) this.robot.port).propeller(100);
+                    ((FredRobotPort) this.robot.port).propeller(100);
 
-            }
-            // Robot has got the ball
-            else {
-                navigation = new PotentialFieldNavigation();
-                navigation.setHeading(destination);
-                navigation.setDestination(null);
-                ((FredRobotPort) this.robot.port).propeller(-50);
-                ((FredRobotPort) this.robot.port).propeller(-50);
-                ((FredRobotPort) this.robot.port).propeller(-50);
-                System.out.println("Yay");
-                this.robot.port.stop();
-                return;
+                }
+                // TODO if the ball is still near the robot after it has kicked move the robot and try to get the ball again.
+                // Use some boolean getA
+                else {
+                    haveBall = 1;
+                    navigation = new AStarNavigation();
+                    navigation.setHeading(destination);
+                    navigation.setDestination(null);
+                    ((FredRobotPort) this.robot.port).propeller(-50);
+                    ((FredRobotPort) this.robot.port).propeller(-50);
+                    ((FredRobotPort) this.robot.port).propeller(-50);
+//                    System.out.println("Yay");
 //                    this.robot.MOTION_CONTROLLER.setHeading(DynamicPointBase.getEnemyGoalPoint());
-            }
-            // Potential field navigation is disabled for now
+                }
+                // Potential field navigation is disabled for now
 //                else {
 //                    navigation = new PotentialFieldNavigation();
 //                    ((FredRobotPort) this.robot.port).propeller(50);
@@ -172,28 +158,44 @@ public class MotionController extends ControllerBase {
 //                    System.out.println("Potential Field Navigation");
 //                }
 
-            navigation.setDestination(new VectorGeometry(destination.x, destination.y));
+            }
+            if (haveBall == 1) {
+                //navigation.setHeading(DynamicPointBase.getEnemyGoalPoint());
+                //System.out.println("Facing enemy goal");
+                navigation = new PotentialFieldNavigation();
+                navigation.setDestination(new VectorGeometry(Constants.PITCH_WIDTH/2, 0));
+                navigation.setHeading(new VectorGeometry(Constants.PITCH_WIDTH/2, 0));
+            }
+            else {
+                System.out.println("I'm in else");
+                navigation.setDestination(new VectorGeometry(destination.x, destination.y));
+            }
 
 
-        }
-        else {
+        } else {
+//            System.out.println("Destination = null");
             return;
         }
-
-        if (this.heading != null) {
+        //System.out.println("Heading: " + this.heading);
+        if(this.heading != null){
             this.heading.recalculate();
             heading = new VectorGeometry(this.heading.getX(), this.heading.getY());
         } else heading = VectorGeometry.fromAngular(us.location.direction, 10, null);
 
 
-        if (this.obstacles != null) {
+
+        if(this.obstacles != null){
             navigation.setObstacles(this.obstacles);
         }
 
 
+        // Make sure that us not nul
         VectorGeometry force = navigation.getForce();
-        if (force == null) {
+        //System.out.println("Force: " + force);
+
+        if(force == null){
             this.robot.port.stop();
+            System.out.println("Force is null");
             return;
         }
 
@@ -202,88 +204,68 @@ public class MotionController extends ControllerBase {
         double factor = 1;
         double rotation = VectorGeometry.signedAngle(robotToPoint, robotHeading);
         // Can throw null without check because null check takes SourceGroup into consideration.
-        if (destination.distance(us.location) < 30) {
+        if(destination.distance(us.location) < 30){
             factor = 0.7;
+        }else if (destination.distance(us.location) < 21){
+            factor = 0.4;
         }
-        if (this.destination != null && us.location.distance(destination) < tolerance) {
+        if(this.destination != null && us.location.distance(destination) < tolerance){
             this.robot.port.stop();
             return;
         }
-
-
 //        strategy.navigationInterface.draw();
 
-        this.robot.drive.move(this.robot.port, us.location, force, rotation, factor);
+        if (haveBall == 1){
+            force =  new VectorGeometry(Constants.PITCH_WIDTH/2, 0);
+            System.out.println("Begin");
+            System.out.println(rotation);
+            robotHeading = VectorGeometry.fromAngular(us.location.direction, 10, null);
+             robotToPoint = VectorGeometry.fromTo(us.location, heading);
+             rotation = VectorGeometry.signedAngle(robotToPoint, robotHeading);
+            System.out.println(rotation);
+            robotHeading = VectorGeometry.fromAngular(us.location.direction, 10, null);
+            robotToPoint = VectorGeometry.fromTo(us.location, heading);
+            rotation = VectorGeometry.signedAngle(robotToPoint, robotHeading);
+            System.out.println(rotation);
+            robotHeading = VectorGeometry.fromAngular(us.location.direction, 10, null);
+            robotToPoint = VectorGeometry.fromTo(us.location, heading);
+            rotation = VectorGeometry.signedAngle(robotToPoint, robotHeading);
+            System.out.println(rotation);
+            System.out.println("End");
+//            System.out.println("robot heading " + robotHeading);
+//            System.out.println("robot to point " + robotToPoint);
 
-    }
-
-    // Not sure if necessary to have a separate from movement rotate class
-    // Use direction to make sure robot is facing the target.
-    private void rotate(Robot us) {
-        NavigationInterface navigation;
-
-        navigation = new PotentialFieldNavigation();
-        navigation.setHeading(new VectorGeometry(Constants.PITCH_WIDTH / 2, 0));
-//            System.out.println("Facing enemy goal");
-            navigation.setDestination(us.location);
-
-        VectorGeometry heading = null;
-
-        VectorGeometry destination = new VectorGeometry(us.location.x, us.location.y);
-
-        if (this.heading != null) {
-            this.heading.recalculate();
-            heading = new VectorGeometry(this.heading.getX(), this.heading.getY());
-            System.out.println("Direction " + us.location.direction);
-        } else {
-            System.out.println("Heading is null ");
-            heading = VectorGeometry.fromAngular(us.location.direction, 10, null);
+            if ( rotation < 0.1 && rotation > -0.05) {
+                this.robot.port.stop();
+                haveBall = 2;
+            }
         }
-
-        VectorGeometry force = navigation.getForce();
-        if (force == null) {
-            this.robot.port.stop();
-            return;
+        if (haveBall == 2){
+//            force = new VectorGeometry(us.location.x, us.location.y);
+            try {
+                System.out.println("Kicking");
+                ((Fred)this.robot).PROPELLER_CONTROLLER.setActive(true);
+                ((FredRobotPort) this.robot.port).propeller(100);
+                ((FredRobotPort) this.robot.port).propeller(100);
+                ((FredRobotPort) this.robot.port).propeller(100);
+                this.robot.port.stop();
+                Thread.sleep(500);
+                ((FredRobotPort) this.robot.port).propeller(0);
+                ((FredRobotPort) this.robot.port).propeller(0);
+                ((FredRobotPort) this.robot.port).propeller(0);
+                haveBall = 0;
+            } catch(InterruptedException ex) {
+                System.out.print("ERROR");
+                Thread.currentThread().interrupt();
+            }
         }
+        if (haveBall == 1){
+            System.out.println("Rotating");
+            this.robot.drive.rotate(this.robot.port, us.location, force, rotation, factor);
+        }else {
+            System.out.println("Moving");
+            this.robot.drive.move(this.robot.port, us.location, force, rotation, factor);
 
-        VectorGeometry robotHeading = VectorGeometry.fromAngular(us.location.direction, 10, null);
-        VectorGeometry robotToPoint = VectorGeometry.fromTo(us.location, heading);
-
-        double factor = 0.7;
-        double rotation = VectorGeometry.signedAngle(robotToPoint, robotHeading);
-        if (this.destination != null && us.location.distance(destination) < tolerance) {
-            this.robot.port.stop();
-            return;
-        }
-        System.out.println("Rotation is: " + rotation);
-
-        if (rotation == 0 ){
-            System.out.println("STOP");
-        }
-
-        this.robot.drive.move(this.robot.port, us.location, force, rotation, factor);
-
-
-    }
-    // Only the actual kicking happens here. To be called either from perform (after check that robot is heading in the right direction) or from rotate
-    private void kick(Robot us){
-        ((FredRobotPort) this.robot.port).propeller(-50);
-        ((FredRobotPort) this.robot.port).propeller(-50);
-        ((FredRobotPort) this.robot.port).propeller(-50);
-        try {
-            Thread.sleep(500);
-            System.out.println("Kicking");
-            ((Fred) this.robot).PROPELLER_CONTROLLER.setActive(true);
-            // Will it crash with 300 spin ? Will it be stronger ? You can do it, robot
-            ((FredRobotPort) this.robot.port).propeller(300);
-            ((FredRobotPort) this.robot.port).propeller(300);
-            ((FredRobotPort) this.robot.port).propeller(300);
-            Thread.sleep(500);
-            ((FredRobotPort) this.robot.port).propeller(0);
-            ((FredRobotPort) this.robot.port).propeller(0);
-            ((FredRobotPort) this.robot.port).propeller(0);
-        } catch (InterruptedException ex) {
-            Thread.currentThread().interrupt();
         }
     }
 }
--- Fred_Stefan_modified/src/strategy/drives/FourWheelHolonomicDrive.java
+++ Fred_Stefan_modified/src/strategy/drives/FourWheelHolonomicDrive.java
@@ -35,19 +35,23 @@ public class FourWheelHolonomicDrive implements DriveInterface{
         left  = left / normalizer * this.MAX_MOTION;
         right = right / normalizer * this.MAX_MOTION;
 
-        /*
-        factor = Math.min(1, factor);
-        double front = dir.y;
-        double left = -dir.x;
-        double back = -dir.y;
-        double right = dir.x;
-        double normalizer = Math.max(Math.max(Math.abs(left), Math.abs(right)), Math.max(Math.abs(front), Math.abs(back)));
-        double lim = this.MAX_MOTION - Math.abs(rotation* this.MAX_ROTATION *factor);
-        normalizer = lim/normalizer*factor;
-        front = front*normalizer + rotation * this.MAX_ROTATION;
-        back  = back*normalizer + rotation * this.MAX_ROTATION;
-        left  = left*normalizer + rotation * this.MAX_ROTATION;
-        right = right*normalizer + rotation * this.MAX_ROTATION;*/
+        ((FourWheelHolonomicRobotPort) port).fourWheelHolonomicMotion(front, -back, -left, right);
+
+    }
+    public void rotate(RobotPort port, DirectedPoint location, VectorGeometry force, double rotation, double factor){
+        assert(port instanceof FourWheelHolonomicRobotPort);
+
+        double front = 0;
+        double back = 30;
+        double left = 30;
+        double right = 30;
+
+//        double normalizer = Math.max(Math.max(Math.abs(left), Math.abs(right)), Math.max(Math.abs(front), Math.abs(back)));
+//        front = front / normalizer * this.MAX_MOTION;
+//        back  = back / normalizer * this.MAX_MOTION;
+//        left  = left / normalizer * this.MAX_MOTION;
+//        right = right / normalizer * this.MAX_MOTION;
+
 
 
         ((FourWheelHolonomicRobotPort) port).fourWheelHolonomicMotion(front, -back, -left, right);
