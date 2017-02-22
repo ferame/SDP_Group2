4d0e63a2fb52cadbba8858e0ba9f48a8d0f12c42
ferame <s1451986@mitford.inf.ed.ac.uk>
2017-02-22 13:20:15 +0000

    Cleaning up the repo

--- Fred_Asta_modified/src/communication/ports/interfaces/ThreeWheelHolonomicRobotPort.java
+++
@@ -1,8 +0,0 @@
-package communication.ports.interfaces;
-
-/**
- * Created by astabogdelyte on 24/01/2017.
- */
-public interface ThreeWheelHolonomicRobotPort {
-    void threeWheelHolonomicMotion(double back, double left, double right);
-}
